7c1a2f093e8f66905624916bef86ea3267cbdb03
majidmohammadian <majeedmohammadian@gmail.com>
2025-11-15 23:13:41 +0330

    added toon service provider

---
+++ src/ToonServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JobMetric\Toon;
+
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class ToonServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-toon')
+            ->hasConfig()
+            ->registerClass('Toon', ToonManager::class);
+    }
+}
