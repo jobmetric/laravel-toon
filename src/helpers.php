f7d8953769c7fea11e8fd258260c349ce8b529b7
majidmohammadian <majeedmohammadian@gmail.com>
2025-11-16 21:26:22 +0330

    added helper file

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,31 @@
 <?php
+
+use JobMetric\Toon\Contracts\ToonManagerInterface;
+
+if (! function_exists('toon_encode')) {
+    /**
+     * Encode PHP data into a TOON string using the application Toon manager.
+     *
+     * @param mixed $data The data to encode.
+     *
+     * @return string
+     */
+    function toon_encode(mixed $data): string
+    {
+        return app(ToonManagerInterface::class)->encode($data);
+    }
+}
+
+if (! function_exists('toon_decode')) {
+    /**
+     * Decode a TOON string back into PHP data using the application Toon manager.
+     *
+     * @param string $toon The TOON text to decode.
+     *
+     * @return mixed
+     */
+    function toon_decode(string $toon): mixed
+    {
+        return app(ToonManagerInterface::class)->decode($toon);
+    }
+}
